cd7b7723ba8768dc2408ccc4551f2d6bd1c5ceb9
tubban1 <liang.rao@gmail.com>
2025-08-21 14:36:23 +0200

    Revert "fix(frontend): 强制 /content/edit/[uuid] 动态渲染 (dynamic='force-dynamic', revalidate=0) 以修复线上直达404"
    This reverts commit daaa4e9b12fdae9f695a00291c129b40ae2fe391.

--- frontend/src/app/content/edit/[uuid]/page.tsx
+++ frontend/src/app/content/edit/[uuid]/page.tsx
@@ -1,8 +1,5 @@
 import EditClient from './client';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 export default function EditPage({ params }: { params: { uuid: string } }) {
 	return <EditClient uuid={params.uuid} />;
 } 
